0b2e900642eea7d879a0850d2b7f5bcc3f68c07e
bhhaskar <bhhaskar1@gmail.com>
2018-09-02 11:19:19 -0500

    new version

--- hi.py
+++ hi.py
@@ -3,4 +3,6 @@ if hungry == "yes":
     print('eat samosa')
     print('ray')
 else:
-    print('do your work')
+    thirsty = input('are you thirsty?')
+    if thirsty == 'yes':
+        print('drink water')
